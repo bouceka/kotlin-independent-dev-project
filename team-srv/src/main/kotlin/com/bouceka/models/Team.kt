6291cf4096825011d0cccd4213619e31957afd68
bouceka <adamm01b@gmail.com>
2022-11-01 18:01:28 -0700

    feat: Integrate repository with the db

--- team-srv/src/main/kotlin/com/bouceka/models/Team.kt
+++ team-srv/src/main/kotlin/com/bouceka/models/Team.kt
@@ -1,8 +1,5 @@
 package com.bouceka.models
 
-import com.bouceka.player.models.Coordinator
-import com.bouceka.player.models.Image
-
 data class Team(
 	val id: String, //TODO: Check if we can use UUID instead
 	val name: String,
@@ -10,6 +7,6 @@ data class Team(
 	val season: String, // TODO: Create enum
 	val year: String, // maybe INT? most likely not
 	val playerLimit: String,
-	val image: Image,
-	val coordinator: Coordinator,
+	val imageId: String,
+	val userId: String,
 )
--- team-srv/src/main/kotlin/com/bouceka/repository/TeamRepository.kt
+++ team-srv/src/main/kotlin/com/bouceka/repository/TeamRepository.kt
@@ -1,4 +1,9 @@
 package com.bouceka.repository
 
-class TeamRepository {
+import com.bouceka.entity.TeamEntity
+import io.micronaut.data.mongodb.annotation.*
+import io.micronaut.data.repository.CrudRepository
+
+@MongoRepository(databaseName = "team")
+interface TeamRepository : CrudRepository<TeamEntity, String> {
 }
