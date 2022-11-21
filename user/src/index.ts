b7dd565e6e629edf5ce0aced7e56081ed282dd89
bouceka <adamm01b@gmail.com>
2022-11-21 15:08:26 -0800

    feat: User service connect to the database

--- user/src/index.ts
+++ user/src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import { json } from 'body-parser';
+import 'express-async-errors'; // handles our async errors
+import mongoose from 'mongoose';
 
-import { currentUserRouter } from '../routes/current-user';
-import { signInRouter } from '../routes/signin';
-import { signOutRouter } from '../routes/signout';
-import { signUpRouter } from '../routes/signup';
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/signin';
+import { signOutRouter } from './routes/signout';
+import { signUpRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
@@ -14,6 +18,22 @@ app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!!!!!');
+app.all('*', async () => {
+  throw new NotFoundError();
 });
+// middleware
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://user-mongo-srv:27017/user'); // connecting to a pod not LOCALHOST!!
+    console.log('Connected to mongodb');
+  } catch (err) {
+    console.log(err);
+  }
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!');
+  });
+};
+
+start();
