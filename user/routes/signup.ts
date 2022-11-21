a4870c11a1d3aa2d7bef69bd9a586c7a57d79c60
bouceka <adamm01b@gmail.com>
2022-11-21 15:07:38 -0800

    chore: User service clean up

--- user/routes/signup.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.post('/api/users/singup', (req, res) => {
-  res.send('<h1> Hi signup! </h1>');
-});
-
-export { router as signUpRouter };
