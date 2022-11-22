262da6b29c494c54137e08c26d4e74fb926ed39d
bouceka <adamm01b@gmail.com>
2022-11-21 17:38:44 -0800

    feat: Create enum classes and clean up in usage

--- registration-srv/src/main/kotlin/com/bouceka/registration/dto/RegistrationDto.kt
+++ registration-srv/src/main/kotlin/com/bouceka/registration/dto/RegistrationDto.kt
@@ -1,10 +1,12 @@
 package com.bouceka.registration.dto
 
+import com.bouceka.registration.models.MatchDay
+import com.bouceka.registration.models.Status
 import java.util.UUID
 
 data class RegistrationDto(
-	val matchDay: String,
-	val status: String,
+	val matchDay: MatchDay,
+	val status: Status,
 	val userId: UUID,
 	val proficiencyId: UUID,
 )
