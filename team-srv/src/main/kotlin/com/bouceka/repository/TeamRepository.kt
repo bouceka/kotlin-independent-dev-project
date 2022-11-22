573b6155c34db4a2809a73dfbabc3484412d59ad
bouceka <adamm01b@gmail.com>
2022-11-21 17:16:11 -0800

    chore: Team entity clean up

--- team-srv/src/main/kotlin/com/bouceka/repository/TeamRepository.kt
+++ team-srv/src/main/kotlin/com/bouceka/repository/TeamRepository.kt
@@ -1,9 +1,9 @@
 package com.bouceka.repository
 
-import com.bouceka.entity.TeamEntity
+import com.bouceka.entity.Team
 import io.micronaut.data.mongodb.annotation.*
 import io.micronaut.data.repository.CrudRepository
 
 @MongoRepository(databaseName = "team")
-interface TeamRepository : CrudRepository<TeamEntity, String> {
+interface TeamRepository : CrudRepository<Team, String> {
 }
